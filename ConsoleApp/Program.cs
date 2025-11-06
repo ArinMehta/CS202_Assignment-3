8eb74184c881537a6ce522d43a5bd4456b61befd
ArinMehta <aagm.rjt@gmail.com>
2025-11-06 21:30:58 +0530

    Code for taking input of two numbers

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,43 @@
-﻿class Program
+﻿using System;
+
+namespace STT
 {
-   static void Main()
+    class Program1
     {
-        Console.WriteLine("Hello! My first C# program is running successfully!");
+        static void Main()
+        {
+            Console.WriteLine("Enter the First Number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the Second Number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = num1 + num2;
+            double difference = num1 - num2;
+            double product = num1 * num2;
+            double quotient;
+            if (num2 != 0)
+            {
+                quotient = num1 / num2;
+            }
+            else
+            {
+                quotient = double.NaN;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Difference: {difference}");
+            Console.WriteLine($"Product: {product}");
+            Console.WriteLine($"Quotient: {quotient}");
+            
+            if (sum % 2 == 0)
+            {
+                Console.WriteLine("The sum is even.");
+            }
+            else
+            {
+                Console.WriteLine("The sum is odd.");
+            }
+        }
     }
 }
--- ConsoleApp/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f489ff4dcd5206225e458dde9bc0683f614f5815")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
